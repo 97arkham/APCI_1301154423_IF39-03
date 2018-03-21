b1c5b2a167909e98f06a0426d70ff9397dac2bb6
97arkham <khairunarkham97@gmail.com>
2018-03-21 18:22:24 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>  
+	<title>basisdata.com</title>
+</head>
+<body>
+	<div class="container"> 
+		<div class="row">
+			<div class="col-sm-6">
+				<img src="img/logo.jpg" width="100" height="90">
+				<h1>Register</h1>
+				<h1>Become a member</h1><br>
+				<p>Ready for join? Please fill the following registration</p>
+				<p>form to become a member of</p>
+				<p>Database Laboratory</p>
+				<p> <strong>  SUBMISSION DEADLINE:</strong> February 26, 2018 </p>
+				<h4>Terms and Conditions</h4>
+				<p>Please, read the terms and conditions below</p>
+				<p><strong>before</strong> filling out the form</p>
+
+				<div class="row">
+					<div class="col-sm-5" style="margin-bottom:5px;">
+						<button type="button" class="btn btn-success btn-block">LOOK INSIDE</button>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-5" style="margin-top:20px;" >
+						<h4>Additional Task</h4>
+					</div>
+				</div>
+				<div class="row"> 
+					<div class="col-sm-2">
+						<button type="button" class="btn btn-success">BIG DATA</button>
+					</div>
+					<div class="col-sm-2" style="margin-left: 10px;">
+						<button type="button" class="btn btn-success">SPATIAL</button>
+					</div>
+					<div class="col-sm-2" >
+						<button type="button" class="btn btn-success">PRODUCT</button>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6"><h1>
+				<div class="row" style="margin-top: 110px; padding-left: 15px;">
+					<h6>Nama Lengkap</h6>
+				</div>
+				<div class="form-group" >
+                	<input type="text" class="form-control" id="usr">
+				</div>
+				<div class="row" style="margin-top: 30px; padding-left: 15px;">
+					<h6>Email</h6>
+				</div>
+				<div class="form-group" >
+                	<input type="text" class="form-control" id="usr">
+				</div>
+				<div class="row" style="margin-top: 30px; padding-left: 15px;">
+					<h6>NIM</h6>
+				</div>
+				<div class="form-group" >
+                	<input type="text" class="form-control" id="usr">
+				</div>
+				<div class="row" style="margin-top: 30px; padding-left: 15px;">
+					<h6>Nomor HP</h6>
+				</div>
+				<div class="form-group" >
+                	<input type="text" class="form-control" id="usr">
+				</div>
+				<div class="row" style="margin-top: 30px; padding-left: 15px;">
+					<h6>ID Line</h6>
+				</div>
+				<div class="form-group" >
+                	<input type="text" class="form-control" id="usr">
+				</div>
+				<div class="row" style="margin-top: 30px; padding-left: 15px;">
+					<h6>Jurusan</h6>
+				</div>
+				<div class="form-group" >
+                	<input type="text" class="form-control" id="usr">
+				</div>
+				<div class="row" style="margin-top: 30px; padding-left: 15px;">
+					<h6>Angkatan</h6>
+				</div>
+				<div class="form-group">
+      				<select class="form-control" id="sel1">
+        			<option>Select One</option>
+        			<option>2015</option>
+        			<option>2016</option>
+       		    	<option>2017</option>
+                	</select>
+                </div>
+                <div class="row" style="margin-top: 30px; padding-left: 0px;"">
+               		 <div class="col-sm-6">
+						<h6>Pilihan Group Pertama</h6>
+						<div class="form-group">
+      						<select class="form-control" id="sel1">
+        					<option>Select One</option>
+        					<option>Big Data</option>
+        					<option>Spatial Database</option>
+       		    			<option>Product</option>
+                			</select>
+                		</div>
+					</div>
+					<div class="col-sm-4">
+						<h6>Persentase Minat</h6>
+						<div class="form-group">
+      						<select class="form-control" id="sel1">
+        					<option>Besar Minatmu</option>
+        					<option>100%</option>
+        					<option>90%</option>
+       		    			<option>80%</option>
+       		    			<option>70%</option>
+       		    			<option>60%</option>
+                			</select>
+                		</div>
+					</div>
+				</div>
+				<h6><strong>Note:</strong>Persentase yang dimaksud adalah presentase minat kamu untuk bergabung pada group pertama</h6>
+				<div class="row" style="margin-top: 30px; padding-left: 0px;">
+               		 <div class="col-sm-6">
+						<h6>Pilihan Group Kedua</h6>
+						<div class="form-group">
+      						<select class="form-control" id="sel1">
+        					<option>Select One</option>
+        					<option>Big Data</option>
+        					<option>Spatial Database</option>
+       		    			<option>Product</option>
+                			</select>
+                		</div>
+					</div>
+					<div class="col-sm-4">
+						<h6>Persentase Minat</h6>
+						<div class="form-group">
+      						<input type="text" class="form-control" id="usr">
+                		</div>
+					</div>
+				</div>
+				<p><strong>Note:</strong>Persentase yang dimaksud adalah presentase minat kamu untuk bergabung pada group pertama</p><br>
+				<div class="form-group">
+  					<label for="comment"><h5>Motivasi</h5></label>
+  					<textarea class="form-control" rows="5" id="comment"></textarea>
+				</div>
+				<p><strong>Note:</strong>Ceritakan apa alasanmu ingin gabung dengan lab basdat dan apa yang kamu dapat dari "additional task" yang telah kamu kerjakan</p><br>
+				<div class="form-group">
+					<label for="essay"><h6>FOTO BEBAS PORTRAIT</h6></label>
+					<input type="file" class="form-control" aria-describedby="filehalp" name="essay" required accept=".zip, .rar, .pdf">
+				</div>
+				<p><strong>Note:</strong>Pastikan file Foto kurang dari 500KB dan dalam format JPG/JPEG/PNG</p><br>
+				<div class="form-group">
+					<label for="essay"><h6>KHS</h6></label>
+					<input type="file" class="form-control" aria-describedby="filehalp" name="essay" required accept=".zip, .rar, .pdf">
+				</div>
+				<p><strong>Note:</strong>Pastikan file KHS kurang dari 500KB dan dalam format .pdf</p><br>
+				<div class="form-group">
+					<label for="essay"><h6>CV</h6></label>
+					<input type="file" class="form-control" aria-describedby="filehalp" name="essay" required accept=".zip, .rar, .pdf">
+				</div>
+				<p><strong>Note:</strong>Pastikan file CV kurang dari 500KB dan dalam format .pdf</p><br>
+				<div class="form-group">
+					<label for="essay"><h6>SCREENSHOT HASIL LIKE & SHARE</h6></label>
+					<input type="file" class="form-control" aria-describedby="filehalp" name="essay" required accept=".zip, .rar, .pdf">
+				</div>
+				<p><strong>Note:</strong>Semua screenshot dimasukkan kedalam 1 file word, pastikan kurang dari 500KB dan dalam format .doc atau .docx</p><br>
+				<div class="form-group">
+					<label for="essay"><h6>ADDITIONAL TASK</h6></label>
+					<input type="file" class="form-control" aria-describedby="filehalp" name="essay" required accept=".zip, .rar, .pdf">
+				</div>
+				<p><strong>Note:</strong>Pastikan file kurang dari 500KB</p><br><br>
+				<p><strong>Note:</strong>Setelah SUBMIT, harap bertanya kepada OA line kami: @basisdatalab untuk memastikan berkasmu sudah terkumpul :)</p>
+				<div class="row"> 
+					
+						<button type="button" class="btn btn-success btn-block">Register NOW</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
